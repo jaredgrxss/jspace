5772907d312ba2d1bdc0cb5009af4af273b94235
Jared <jaredgrossthe@gmail.com>
2023-03-05 19:22:56 -0600

    Navbar constructed

--- app.js
+++ app.js
@@ -7,19 +7,21 @@ const session = require('express-session');
 const MongoSessionStorage = require('connect-mongodb-session')(session);
 
 MONGODB_URI = 'mongodb+srv://jaredgrxss:jared1939@seniorprojectdb.kfltxc7.mongodb.net/market?retryWrites=true&w=majority'
-
-//templating engine setup
-app.set('view engine', 'ejs');
-app.set('views','./views');
-
 //storing sessions on our db
 const store = new MongoSessionStorage({
     uri: MONGODB_URI,
     collection: 'sessions'
 });
 
+
+//templating engine setup
+app.set('view engine', 'ejs');
+app.set('views','./views');
+
 //routes
 const marketplace = require('./routes/market');
+const authentication = require('./routes/authentication');
+const admin = require('./routes/admin');
 const errorPage = require('./controllers/error');
 
 //parser to parse all request bodies
@@ -31,12 +33,22 @@ app.use(express.static(path.join(__dirname,'public')));
 //session middle-ware
 app.use(session({secret: 'CSCI487SENIORPROJECTSECRETKEY', resave: false, saveUninitialized: false, store: store}));
 
+//local variables for sessions
+app.use((req, res, next) => {
+    res.locals.isAuth = req.session.isLoggedIn;
+    next();
+})
+
 //funnel request to search for routes
 app.use(marketplace);
+app.use(authentication);
+app.use(admin);
 
 //error 404 page
 app.use('/', errorPage.Error);
 
+
+mongoose.set('strictQuery', false);
 mongoose.connect(MONGODB_URI)
 .then(result => {
     app.listen(8000);
